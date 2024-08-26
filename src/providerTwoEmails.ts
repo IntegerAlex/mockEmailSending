f7df8803ec7745b76bab2d88aef08c6f1d39e48e
akshat k <akshatkot@gmail.com>
2024-08-27 01:42:09 +0530

    Removed testing mails and added different files for emails

---
+++ src/providerTwoEmails.ts
@@ -0,0 +1,26 @@
+// providerTwoEmails.ts
+
+export const providerTwoEmails: string[] = [
+    'user22@example.com',
+    'user23@example.com',
+    'user24@example.com',
+    'user25@example.com',
+    'user26@example.com',
+    'user27@example.com',
+    'user28@example.com',
+    'user29@example.com',
+    'user30@example.com',
+    'user31@example.com',
+    'user32@example.com',
+    'user33@example.com',
+    'user34@example.com',
+    'user35@example.com',
+    'user36@example.com',
+    'user37@example.com',
+    'user38@example.com',
+    'user39@example.com',
+    'user40@example.com',
+    'user41@example.com',
+    'user42@example.com'
+];
+
